80dadd1b6f1ff6e6be431eaa3e3d16d995f69521
Alejandro Durando <ale.durando@outlook.com>
2025-04-01 15:11:42 -0300

    Inicial

---
+++ tp2_1_.c
@@ -0,0 +1,26 @@
+// codigo a completar
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20
+
+
+int main(){
+    
+srand(time(NULL)); //para randomizar los numeros del vector.
+
+int i;
+double vt[N];
+    
+
+    for(i = 0;i<N; i++)
+        {
+            vt[i]=1+rand()%100;
+
+            printf(" %f ", vt[i]);
+        }
+
+
+    return 0;
+    }
+
